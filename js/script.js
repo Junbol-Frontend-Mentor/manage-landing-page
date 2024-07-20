66790331faf665765d17c6762c9362f2bacff419
Junier Bolivar <junierba@yahoo.co.uk>
2024-07-20 14:34:53 +0200

    done

--- js/script.js
+++ js/script.js
@@ -11,28 +11,37 @@ document.addEventListener('DOMContentLoaded', function () {
     nav.classList.toggle('hidden');
   });
 
-  // Flowbite carousel functionality
-  const carouselElement = document.getElementById('default-carousel');
-
-  const items = [
-    { position: 0, el: document.querySelector('[data-carousel-item="0"]') },
-    { position: 1, el: document.querySelector('[data-carousel-item="1"]') },
-    { position: 2, el: document.querySelector('[data-carousel-item="2"]') },
-    { position: 3, el: document.querySelector('[data-carousel-item="3"]') },
-  ];
-
-  const options = {
-    defaultPosition: 0,
-    interval: 3000,
-    indicators: {
-      items: [
-        { position: 0, el: document.querySelector('[data-carousel-slide-to="0"]') },
-        { position: 1, el: document.querySelector('[data-carousel-slide-to="1"]') },
-        { position: 2, el: document.querySelector('[data-carousel-slide-to="2"]') },
-        { position: 3, el: document.querySelector('[data-carousel-slide-to="3"]') },
-      ],
-    },
-  };
-
-  const carousel = new flowbite.Carousel(carouselElement, items, options);
 });
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName('mySlides');
+  let dots = document.getElementsByClassName('dot');
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+}
